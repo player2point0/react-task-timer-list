2bc60339dd5b410e45fc414350c69c1dbbe1a1b4
nathan <nathannash11@gmail.com>
2019-11-30 20:50:03 +0000

    fixed broken id gen

--- src/task.js
+++ src/task.js
@@ -1,13 +1,19 @@
 class TaskContainer {
     constructor(name, duration){
-        this.id = Date.now()+"task";
+        this.id = this.uuidv4();
         this.name = name;
         this.duration = duration;
         this.started = false;
         this.isViewing = false;
     }
 
-
+    //unique id generator
+    uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
+      }
 }
 
 export default TaskContainer;
