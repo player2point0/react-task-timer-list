cb62925dd8d21bae7e517aa550ea3d453bcdaaf2
nathan <nathannash11@gmail.com>
2020-01-02 21:08:04 +0000

    ui work

--- src/index.js
+++ src/index.js
@@ -146,7 +146,7 @@ class TaskController extends React.Component {
         let defaultTasks = [
             new TaskContainer("task1", 60),
             //new TaskContainer("task2", 123),
-            //new TaskContainer("task3", 3600)
+            new TaskContainer("task3", 3600)
         ];
         this.state = {
             time: 0,
