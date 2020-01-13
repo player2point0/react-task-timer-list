3f5f8b57b3dc0391525b748b5c411ce835756161
nathan <nathannash11@gmail.com>
2020-01-13 16:04:21 +0000

    lifted up pomodoro state to fix bug

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,88 +1,6 @@
 import React from 'react';
 
-const WORK_TIME = 25 * 60;
-const BREAK_TIME = 5 * 60;
-
 export default class Pomodoro extends React.Component{
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            startedWork: false,
-            startedBreak: false,
-            workTimeRemaining: WORK_TIME,
-            breakTimeRemaining: BREAK_TIME, 
-        };
-
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-
-    tick() {
-        let activeTask = false;
-        const tempTasks = this.props.tasks;
-        
-        //check for an active task
-        for(let i = 0; i < tempTasks.length; i++) {
-            //check for a running task
-            if(tempTasks[i].started && !tempTasks[i].paused){
-                activeTask = true;
-                break;
-            }
-        }
-
-        //nothing to do - afk
-        if(!activeTask) return;
-
-        //if not started and have an active task
-        //then start the work time or the break timer
-        if(!this.state.startedWork){
-            this.setState(state => ({
-                startedWork: true,
-                startedBreak: false,
-                workTimeRemaining: WORK_TIME        
-            }));
-        }
-
-        //perform the ticks and other logic
-        else{
-            if(this.state.workTimeRemaining > 0){
-                this.setState(state => ({
-                    workTimeRemaining: this.state.workTimeRemaining - 1
-                }));
-            }
-
-            else{
-
-                if(!this.state.startedBreak){
-                    //call callback to show notification
-
-                    this.setState(state => ({
-                        startedBreak: true,
-                        breakTimeRemaining: BREAK_TIME
-                    }));
-                }
-
-                else if(this.state.breakTimeRemaining > 0){
-                    this.setState(state => ({
-                        workTimeRemaining: this.state.workTimeRemaining - 1
-                    }));
-                }
-
-                //break finished
-                else{
-                    //call other callback to show notification
-                    
-                    this.setState(state => ({
-                        startedWork: false,
-                        startedBreak: false,    
-                        //workTimeRemaining: WORK_TIME,
-                        //breakTimeRemaining: BREAK_TIME,
-                    }));
-                }
-            }
-        }
-    }
-
     formatTime(time){
         let mins = String(Math.floor((time) / 60));
         let seconds  = String(Math.floor((time - (mins * 60))));
@@ -94,16 +12,11 @@ export default class Pomodoro extends React.Component{
     }
 
     render() {
-
-
-
-
-
         return (
             <div>
                 <h2>time</h2>
-                <h2>work time {this.formatTime(this.state.workTimeRemaining)}</h2>
-                <h2>break time {this.formatTime(this.state.breakTimeRemaining)}</h2>
+                <h2>work time {this.formatTime(this.props.workTimeRemaining)}</h2>
+                <h2>break time {this.formatTime(this.props.breakTimeRemaining)}</h2>
             </div>
         );
     }
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import Pomodoro from './Pomodoro.js';
 
+const WORK_TIME = 25 * 60;
+const BREAK_TIME = 5 * 60;
+
 export default class SideBar extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            showSideBar: false
+            showSideBar: false,
+            pomodoro: {
+                startedWork: false,
+                startedBreak: false,
+                workTimeRemaining: WORK_TIME,
+                breakTimeRemaining: BREAK_TIME, 
+            },
         };
 
         this.showSideBar = this.showSideBar.bind(this);
         this.hideSideBar = this.hideSideBar.bind(this);
+
+        this.interval = setInterval(() => this.pomodoroTick(), 1000);
     }
 
     showSideBar(){
@@ -22,6 +33,68 @@ export default class SideBar extends React.Component {
     hideSideBar(){
         this.setState(state => ({
             showSideBar: false
+        }));
+    }
+
+    pomodoroTick() {
+        let activeTask = false;
+        const tempTasks = this.props.tasks;
+        const pomodoro = this.state.pomodoro;
+        
+        //check for an active task
+        for(let i = 0; i < tempTasks.length; i++) {
+            //check for a running task
+            if(tempTasks[i].started && !tempTasks[i].paused){
+                activeTask = true;
+                break;
+            }
+        }
+
+        //nothing to do - afk
+        if(!activeTask) return;
+
+        //if not started and have an active task
+        //then start the work time or the break timer
+        if(!this.state.pomodoro.startedWork){
+            pomodoro.startedWork = true;
+            pomodoro.startedBreak = false;
+            pomodoro.workTimeRemaining = WORK_TIME;
+        }
+
+        //perform the ticks and other logic
+        else{
+            if(this.state.pomodoro.workTimeRemaining > 0){
+                pomodoro.workTimeRemaining = this.state.pomodoro.workTimeRemaining - 1;
+            }
+
+            else{
+
+                if(!this.state.pomodoro.startedBreak){
+                    //call callback to show notification
+
+                    pomodoro.startedBreak = true;
+                    pomodoro.breakTimeRemaining = BREAK_TIME;
+                }
+
+                else if(this.state.pomodoro.breakTimeRemaining > 0){
+                    
+                    pomodoro.workTimeRemaining = this.state.pomodoro.workTimeRemaining - 1;
+                }
+
+                //break finished
+                else{
+                    //call other callback to show notification
+                    
+                    pomodoro.startedWork = false;
+                    pomodoro.startedBreak = false;
+                    //pomodoro.breakTimeRemaining = BREAK_TIME;
+                    //pomodoro.workTimeRemaining = WORK_TIME;
+                }
+            }
+        }
+
+        this.setState(state => ({
+            pomodoro: pomodoro
         }));
     }
 
@@ -42,7 +115,8 @@ export default class SideBar extends React.Component {
                     <h1>stats</h1>
                     <h1>pomodoro</h1>
                     <Pomodoro
-                        tasks={this.props.tasks}
+                        workTimeRemaining={this.state.pomodoro.workTimeRemaining}
+                        breakTimeRemaining={this.state.pomodoro.breakTimeRemaining}
                     />
                 </div>
                 <div className="closeSideBar" onClick={this.hideSideBar}>
