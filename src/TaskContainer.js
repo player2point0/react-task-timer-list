3ed37b871dc52918fd44ff845a7abc7c17b2684a
nathan <nathannash11@gmail.com>
2020-01-15 23:21:23 +0000

    started stats

--- src/TaskContainer.js
+++ src/TaskContainer.js
@@ -5,9 +5,33 @@ class TaskContainer {
         //time in seconds
         this.totalDuration = duration;
         this.remainingTime = duration;
+        this.additionalTime = Number(duration / 2);
+        this.timeUp = false;
         this.started = false;
         this.paused = false;
         this.isViewing = false;
+        this.stats = {
+          timeAdded: 0,
+          timesPaused: 0,
+        };
+
+        this.test = "testing";
+    }
+
+    addTime(){
+      let extraTime = Number(this.remainingTime) + this.additionalTime;
+      this.remainingTime = extraTime;
+      this.timeUp = false;
+      
+      this.stats.timeAdded += extraTime;
+    }
+
+    pause(){
+      this.paused = true;
+    }
+
+    unPause(){
+      this.paused = false;
     }
 
     //unique id generator
