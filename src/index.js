bd1cbd9128b8ec5ddd84e7474ebcf46b3bb5446b
nathan <nathannash11@gmail.com>
2020-03-15 18:29:53 +0000

    firebase login ui

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import SideBar from './SideBar.js'
 import * as serviceWorker from './serviceWorker.js';
 import {getDateStr, sendNotification} from './Ultility.js';
 
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+var firebaseui = require('firebaseui');
+
 const SAVE_INTERVAL = 60;
 
 class TaskController extends React.Component {
@@ -377,7 +383,32 @@ requestNotifications();
 
 serviceWorker.register();
 
-//firebase 
+//firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAxGvolzw0mVmK5rjfLh9hT3OBgeihjv4o",
+    authDomain: "react-task-app-d7d0a.firebaseapp.com",
+    databaseURL: "https://react-task-app-d7d0a.firebaseio.com",
+    projectId: "react-task-app-d7d0a",
+    storageBucket: "react-task-app-d7d0a.appspot.com",
+    messagingSenderId: "552127368915",
+    appId: "1:552127368915:web:e2566ec7eca32b03bd8e26",
+    measurementId: "G-470KV7252P"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+});
+
+
 
 ReactDOM.render(
     <TaskController />,
