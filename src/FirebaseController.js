8a7609b194c88233e347b64f6874a6c31a004f83
nathan <nathannash11@gmail.com>
2020-03-31 00:24:21 +0100

    refactored login

---
+++ src/FirebaseController.js
@@ -0,0 +1,452 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import TaskController from "./TaskController.js";
+import TaskContainer from "./TaskContainer.js";
+import {formatDayMonth, requestNotifications, sendNotification} from "./Ultility";
+import Task from "./ReactTask";
+
+const SAVE_INTERVAL = 60;
+
+//firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAxGvolzw0mVmK5rjfLh9hT3OBgeihjv4o",
+    authDomain: "react-task-app-d7d0a.firebaseapp.com",
+    databaseURL: "https://react-task-app-d7d0a.firebaseio.com",
+    projectId: "react-task-app-d7d0a",
+    storageBucket: "react-task-app-d7d0a.appspot.com",
+    messagingSenderId: "552127368915",
+    appId: "1:552127368915:web:e2566ec7eca32b03bd8e26",
+    measurementId: "G-470KV7252P",
+};
+
+// Configure FirebaseUI.
+const uiConfig = {
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            // Avoid redirects after sign-in.
+            signInSuccessWithAuthResult: () => false
+        },
+    };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+export default class FirebaseController extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tasks: [],//this.loadLocalTasks(),
+            showAuthHtml: firebase.auth().currentUser == null,
+            time: 0,
+            removeTaskId: "",
+            setSaveAllTasks: false,
+        }
+
+        this.saveAllTasks = this.saveAllTasks.bind(this);
+        this.addTask = this.addTask.bind(this);
+        this.taskOnClick = this.taskOnClick.bind(this);
+        this.taskUp = this.taskUp.bind(this);
+        this.taskDown = this.taskDown.bind(this);
+        this.startTask = this.startTask.bind(this);
+        this.finishTask = this.finishTask.bind(this);
+        this.addTime = this.addTime.bind(this);
+        this.removeTaskWithId = this.removeTaskWithId.bind(this);
+        this.setLoadedTasks = this.setLoadedTasks.bind(this);
+        this.firebaseSaveTask = this.firebaseSaveTask.bind(this);
+        this.firebaseGetAllTasks = this.firebaseGetAllTasks.bind(this);
+        this.loadLocalTasks = this.loadLocalTasks.bind(this);
+        this.parseSavedTasks = this.parseSavedTasks.bind(this);
+
+        this.db = firebase.firestore();
+    }
+
+    //update all the tasks which are started
+    tick() {
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = 0; i < updatedTasks.length; i++) {
+            if (updatedTasks[i].started && !updatedTasks[i].paused) {
+                updatedTasks[i].remainingTime--;
+                if (updatedTasks[i].remainingTime <= 0) {
+                    updatedTasks[i].remainingTime = 0;
+
+                    if (!updatedTasks[i].timeUp) {
+                        sendNotification("task out of time", "");
+                        updatedTasks[i].isViewing = true;
+                        updatedTasks[i].timeUp = true;
+                        sendNotification("Task time finished", updatedTasks[i].name);
+                        this.saveAllTasks();
+                    }
+                }
+            }
+        }
+
+        this.setState(state => ({
+            tasks: updatedTasks,
+            time: state.time + 1,
+        }));
+
+        //save all tasks every n ticks
+        if (this.state.time % SAVE_INTERVAL === 0) {
+            this.setState({ setSaveAllTasks: true });
+        }
+    }
+
+    addTask(currentState) {
+        requestNotifications();
+
+        //todo improve this
+        if (
+            !currentState.newTaskName ||
+            !currentState.newTaskHours ||
+            !currentState.newTaskHours
+        )
+            return;
+        if (isNaN(currentState.newTaskHours) || isNaN(currentState.newTaskMins)) return;
+        if (currentState.newTaskHours < 0 || currentState.newTaskMins < 0) return;
+
+        let newTaskDuration = currentState.newTaskHours * 3600 + currentState.newTaskMins * 60;
+        let currentDate = new Date(); //todo change so works with multi days
+
+        let newTask = new TaskContainer(
+            currentState.newTaskName,
+            newTaskDuration,
+            currentDate
+        );
+
+        this.setState(state => ({
+            tasks: state.tasks.concat(newTask),
+            setSaveAllTasks: true,
+        }));
+    }
+
+    //display the selected task
+    taskOnClick(id) {
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = 0; i < updatedTasks.length; i++) {
+            if (updatedTasks[i].id === id) {
+                updatedTasks[i].isViewing = !updatedTasks[i].isViewing;
+                break;
+            }
+        }
+
+        this.setState({
+            tasks: updatedTasks,
+        });
+    }
+
+    //move the selected task up in the UI and nearer to the front in the list
+    taskUp(id) {
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = 0; i < updatedTasks.length; i++) {
+            if (updatedTasks[i].id === id) {
+                if (i - 1 < 0) return; //already in first place
+
+                //swap
+                let tempTask = updatedTasks[i];
+                updatedTasks[i] = updatedTasks[i - 1];
+                updatedTasks[i - 1] = tempTask;
+
+                break;
+            }
+        }
+
+        this.setState({
+            tasks: updatedTasks,
+            setSaveAllTasks: true,
+        });
+    }
+
+    //move the selected task down in the UI and nearer to the end in the list
+    taskDown(id) {
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = 0; i < updatedTasks.length; i++) {
+            if (updatedTasks[i].id === id) {
+                if (i + 1 >= updatedTasks.length) return; //already in last place
+
+                //swap
+                let tempTask = updatedTasks[i];
+                updatedTasks[i] = updatedTasks[i + 1];
+                updatedTasks[i + 1] = tempTask;
+
+                break;
+            }
+        }
+
+        this.setState({
+            tasks: updatedTasks,
+            setSaveAllTasks: true,
+        });
+    }
+
+    startTask(id) {
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = updatedTasks.length - 1; i >= 0; i--) {
+            if (updatedTasks[i].id === id) {
+
+                if (updatedTasks[i].started) {
+                    if (updatedTasks[i].remainingTime >= 0) {
+                        if (updatedTasks[i].paused) updatedTasks[i].unPause();
+                        else updatedTasks[i].pause();
+                    }
+                }
+
+                else{
+                    updatedTasks[i].start();
+                }
+
+                break;
+            }
+        }
+
+        this.setState({
+            tasks: updatedTasks,
+            setSaveAllTasks: true,
+        });
+    }
+
+    finishTask(id) {
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = updatedTasks.length - 1; i >= 0; i--) {
+            if (updatedTasks[i].id === id) {
+                updatedTasks[i].finish();
+
+                this.setState({
+                    tasks: updatedTasks,
+                    setSaveAllTasks: true,
+                    removeTaskId: updatedTasks[i].id
+                });
+
+                return;
+            }
+        }
+    }
+
+    removeTaskWithId(id){
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = updatedTasks.length - 1; i >= 0; i--) {
+            if (updatedTasks[i].id === id) {
+                this.saveStatTask(updatedTasks[i]);
+                updatedTasks.splice(i, 1);
+
+
+                console.log("remove");
+                break;
+            }
+        }
+
+        this.setState({
+            tasks: updatedTasks,
+            setSaveAllTasks: true,
+        });
+    }
+
+    addTime(id) {
+        const updatedTasks = this.state.tasks.slice();
+        for (let i = 0; i < updatedTasks.length; i++) {
+            if (updatedTasks[i].id === id) {
+                updatedTasks[i].addTime();
+                break;
+            }
+        }
+
+        this.setState({
+            tasks: updatedTasks,
+            setSaveAllTasks: true,
+        });
+    }
+
+    //saves the task whenever finished
+    saveStatTask(task) {
+        let dateStr = formatDayMonth(task.dateCreated); // saves the task to the day it was started on
+
+        //get the saved stats for today if any
+        let savedStats = JSON.parse(localStorage.getItem(dateStr));
+        if (savedStats == null) savedStats = [];
+        savedStats.push(task);
+
+        localStorage.setItem(dateStr, JSON.stringify(savedStats));
+    }
+
+    //save all tasks
+    saveAllTasks() {
+        localStorage.setItem("tasks", JSON.stringify(this.state.tasks));
+
+        this.state.tasks.forEach(task => {
+            this.firebaseSaveTask(task);
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+        if(this.state.removeTaskId){
+            this.removeTaskWithId(this.state.removeTaskId);
+            this.setState({removeTaskId: ""});
+        }
+
+        if (this.state.setSaveAllTasks) {
+            this.saveAllTasks();
+            this.setState({ setSaveAllTasks: false });
+        }
+    }
+
+    // Listen to the Firebase Auth state and set the local state.
+    //interval for the tick method, called when changes are made to the props
+    componentDidMount() {
+        this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(this.setLoadedTasks);
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    // Make sure we un-register Firebase observers when the component unmounts.
+    componentWillUnmount() {
+        this.unregisterAuthObserver();
+        this.interval();
+    }
+
+    // login / signup / guest
+    setLoadedTasks(user) {
+        let newState = {};
+        let scope = this;
+
+        if (user) {
+            //get saved tasks
+            //display task controller
+            this.firebaseGetAllTasks(function (savedTasks) {
+                scope.setState(state => ({
+                    tasks: scope.parseSavedTasks(savedTasks),
+                    showAuthHtml: false,
+                }));
+            });
+        } else {
+            //tasks from local storage
+            this.setState(state => ({
+                tasks: this.loadLocalTasks(),
+                showAuthHtml: true,
+            }));
+        }
+    }
+
+    firebaseSaveTask(task) {
+        const currentUser = firebase.auth().currentUser;
+
+        if (!currentUser) {
+            console.error("not logged in");
+            return;
+        }
+
+        this.db.collection("tasks")
+            .doc(task.id)
+            .set({
+                id: task.id,
+                name: task.name,
+                dateCreated: task.dateCreated,
+                totalTime: task.totalTime,
+                remainingTime: task.remainingTime,
+                addTimeAmt: task.addTimeAmt,
+                timeUp: task.timeUp,
+                started: task.started,
+                finished: task.finished,
+                paused: task.paused,
+                isViewing: task.isViewing,
+                stats: {
+                    timeAdded: task.stats.timeAdded,
+                    dateStarted: task.stats.dateStarted,
+                    dateFinished: task.stats.dateFinished,
+                    pauseDates: task.stats.pauseDates,
+                    unpauseDates: task.stats.unpauseDates,
+                },
+                userId: currentUser.uid,
+            })
+            .then(value => console.log("saved task successfully"))
+            .catch(reason => console.error("error saving task" + reason));
+    }
+
+    firebaseGetAllTasks(callback) {
+        const currentUser = firebase.auth().currentUser;
+
+        if (!currentUser) {
+            console.error("not logged in");
+            return;
+        }
+
+        this.db.collection("tasks")
+            .where("userId", "==", currentUser.uid)
+            .where("finished", "==", false)
+            .get()
+            .then(function (querySnapshot) {
+                let savedTasks = [];
+                let tempTask;
+
+                querySnapshot.forEach(function (doc) {
+                    tempTask = doc.data();
+                    tempTask.dateCreated = doc.data().dateCreated.toDate();
+                    savedTasks.push(tempTask);
+                });
+
+                callback(savedTasks);
+            })
+            .catch(function (error) {
+                console.log("Error getting documents: ", error);
+            });
+    }
+
+    //load any saved tasks
+    loadLocalTasks() {
+        let localSavedTasks = JSON.parse(localStorage.getItem("tasks"));
+        return this.parseSavedTasks(localSavedTasks);
+    }
+
+    parseSavedTasks(savedTasks) {
+        let outputSavedTasks = [];
+        let newTask;
+
+        if (savedTasks == null) savedTasks = [];
+
+        for (let i = 0; i < savedTasks.length; i++) {
+            newTask = new TaskContainer(null, null, null, savedTasks[i]);
+
+            outputSavedTasks.push(newTask);
+        }
+
+        return outputSavedTasks;
+    }
+
+    render() {
+        let authHtml;
+
+        if(this.state.showAuthHtml) authHtml = <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>;
+
+        return (
+            <div>
+                {authHtml}
+                <TaskController
+                    firebaseSaveTask={this.firebaseSaveTask}
+                    firebaseGetAllTasks={this.firebaseGetAllTasks}
+                    tasks={this.state.tasks}
+                    addTask={this.addTask}
+                    taskOnClick={this.taskOnClick}
+                    taskUp={this.taskUp}
+                    taskDown={this.taskDown}
+                    startTask={this.startTask}
+                    finishTask={this.finishTask}
+                    addTime={this.addTime}
+                />
+            </div>
+        );
+    }
+
+}
--- src/TaskController.js
+++ src/TaskController.js
@@ -9,18 +9,13 @@ import {
 	requestNotifications,
 } from "./Ultility.js";
 
-const SAVE_INTERVAL = 60;
 
 export default class TaskController extends React.Component {
 	constructor(props) {
 		super(props);
 		//store the tasks
 		this.state = {
-			time: 0,
 			showTaskForm: false,
-			setSaveAllTasks: false,
-			removeTaskId: "",
-			tasks: this.props.savedTasks,
 		};
 
 		this.toggleTaskForm = this.toggleTaskForm.bind(this);
@@ -28,69 +23,6 @@ export default class TaskController extends React.Component {
 		this.handleNewTaskHoursChange = this.handleNewTaskHoursChange.bind(this);
 		this.handleNewTaskMinsChange = this.handleNewTaskMinsChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.addTask = this.addTask.bind(this);
-		this.taskOnClick = this.taskOnClick.bind(this);
-		this.taskUp = this.taskUp.bind(this);
-		this.taskDown = this.taskDown.bind(this);
-		this.startTask = this.startTask.bind(this);
-		this.finishTask = this.finishTask.bind(this);
-		this.addTime = this.addTime.bind(this);
-		this.saveAllTasks = this.saveAllTasks.bind(this);
-		this.removeTaskWithId = this.removeTaskWithId.bind(this);
-
-		this.interval = setInterval(() => this.tick(), 1000);
-	}
-
-	//update all the tasks which are started
-	tick() {
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = 0; i < updatedTasks.length; i++) {
-			if (updatedTasks[i].started && !updatedTasks[i].paused) {
-				updatedTasks[i].remainingTime--;
-				if (updatedTasks[i].remainingTime <= 0) {
-					updatedTasks[i].remainingTime = 0;
-
-					if (!updatedTasks[i].timeUp) {
-						sendNotification("task out of time", "");
-						updatedTasks[i].isViewing = true;
-						updatedTasks[i].timeUp = true;
-						sendNotification("Task time finished", updatedTasks[i].name);
-						this.saveAllTasks();
-					}
-				}
-			}
-		}
-
-		this.setState(state => ({
-			tasks: updatedTasks,
-			time: state.time + 1,
-		}));
-
-		//save all tasks every n ticks
-		if (this.state.time % SAVE_INTERVAL === 0) {
-			this.setState({ setSaveAllTasks: true });
-		}
-	}
-
-	//save all tasks
-	saveAllTasks() {
-		localStorage.setItem("tasks", JSON.stringify(this.state.tasks));
-
-		this.state.tasks.forEach(task => {
-			this.props.firebaseSaveTask(task);
-		});
-	}
-
-	//saves the task whenever finished
-	saveStatTask(task) {
-		let dateStr = formatDayMonth(task.dateCreated); // saves the task to the day it was started on
-
-		//get the saved stats for today if any
-		let savedStats = JSON.parse(localStorage.getItem(dateStr));
-		if (savedStats == null) savedStats = [];
-		savedStats.push(task);
-
-		localStorage.setItem(dateStr, JSON.stringify(savedStats));
 	}
 
 	//display the add task inputs
@@ -98,20 +30,6 @@ export default class TaskController extends React.Component {
 		this.setState(state => ({
 			showTaskForm: !state.showTaskForm,
 		}));
-	}
-
-	//interval for the tick method, called when changes are made to the props
-	componentDidUpdate() {
-		if(this.state.removeTaskId){
-			this.removeTaskWithId(this.state.removeTaskId);
-			this.setState({removeTaskId: ""});
-		}
-
-		if (this.state.setSaveAllTasks) {
-			console.log("save tasks");
-			this.saveAllTasks();
-			this.setState({ setSaveAllTasks: false });
-		}
 	}
 
 	//methods for the new task input
@@ -126,176 +44,13 @@ export default class TaskController extends React.Component {
 	}
 	handleSubmit(e) {
 		e.preventDefault();
-		this.addTask();
-	}
-
-	addTask() {
-		requestNotifications();
-
-		//todo improve this
-		if (
-			!this.state.newTaskName ||
-			!this.state.newTaskHours ||
-			!this.state.newTaskHours
-		)
-			return;
-		if (isNaN(this.state.newTaskHours) || isNaN(this.state.newTaskMins)) return;
-		if (this.state.newTaskHours < 0 || this.state.newTaskMins < 0) return;
-
-		let newTaskDuration =
-			this.state.newTaskHours * 3600 + this.state.newTaskMins * 60;
-		let currentDate = new Date(); //todo change so works with multi days
-
-		let newTask = new TaskContainer(
-			this.state.newTaskName,
-			newTaskDuration,
-			currentDate
-		);
-
-		this.setState(state => ({
-			tasks: state.tasks.concat(newTask),
-			addTask: false,
-		}));
-
-		//clear the inputs
-		this.setState(state => ({
+		const currentState = this.state;
+		this.props.addTask(currentState);
+		this.setState(state =>({
 			newTaskName: "",
 			newTaskHours: "0",
 			newTaskMins: "0",
-			setSaveAllTasks: true,
 		}));
-	}
-
-	//display the selected task
-	taskOnClick(id) {
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = 0; i < updatedTasks.length; i++) {
-			if (updatedTasks[i].id === id) {
-				updatedTasks[i].isViewing = !updatedTasks[i].isViewing;
-				break;
-			}
-		}
-
-		this.setState({
-			tasks: updatedTasks,
-		});
-	}
-
-	//move the selected task up in the UI and nearer to the front in the list
-	taskUp(id) {
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = 0; i < updatedTasks.length; i++) {
-			if (updatedTasks[i].id === id) {
-				if (i - 1 < 0) return; //already in first place
-
-				//swap
-				let tempTask = updatedTasks[i];
-				updatedTasks[i] = updatedTasks[i - 1];
-				updatedTasks[i - 1] = tempTask;
-
-				break;
-			}
-		}
-
-		this.setState({
-			tasks: updatedTasks,
-			setSaveAllTasks: true,
-		});
-	}
-
-	//move the selected task down in the UI and nearer to the end in the list
-	taskDown(id) {
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = 0; i < updatedTasks.length; i++) {
-			if (updatedTasks[i].id === id) {
-				if (i + 1 >= updatedTasks.length) return; //already in last place
-
-				//swap
-				let tempTask = updatedTasks[i];
-				updatedTasks[i] = updatedTasks[i + 1];
-				updatedTasks[i + 1] = tempTask;
-
-				break;
-			}
-		}
-
-		this.setState({
-			tasks: updatedTasks,
-			setSaveAllTasks: true,
-		});
-	}
-
-	startTask(id) {
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = updatedTasks.length - 1; i >= 0; i--) {
-			if (updatedTasks[i].id === id) {
-
-				if (updatedTasks[i].started) {
-					if (updatedTasks[i].remainingTime >= 0) {
-						if (updatedTasks[i].paused) updatedTasks[i].unPause();
-						else updatedTasks[i].pause();
-					}
-				}
-
-				else{
-					updatedTasks[i].start();
-				}
-
-				break;
-			}
-		}
-
-		this.setState({
-			tasks: updatedTasks,
-			setSaveAllTasks: true,
-		});
-	}
-
-	finishTask(id) {
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = updatedTasks.length - 1; i >= 0; i--) {
-			if (updatedTasks[i].id === id) {
-				updatedTasks[i].finish();
-
-				this.setState({
-					tasks: updatedTasks,
-					setSaveAllTasks: true,
-					removeTaskId: updatedTasks[i].id
-				});
-				return;
-			}
-		}
-	}
-
-	removeTaskWithId(id){
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = updatedTasks.length - 1; i >= 0; i--) {
-			if (updatedTasks[i].id === id) {
-				this.saveStatTask(updatedTasks[i]);
-				updatedTasks.splice(i, 1);
-				break;
-			}
-		}
-
-		this.setState({
-			tasks: updatedTasks,
-			setSaveAllTasks: true,
-		});
-	}
-
-	addTime(id) {
-		const updatedTasks = this.state.tasks.slice();
-		for (let i = 0; i < updatedTasks.length; i++) {
-			if (updatedTasks[i].id === id) {
-				updatedTasks[i].addTime();
-				break;
-			}
-		}
-
-		this.setState({
-			tasks: updatedTasks,
-			setSaveAllTasks: true,
-		});
 	}
 
 	render() {
@@ -344,11 +99,12 @@ export default class TaskController extends React.Component {
 
 		return (
 			<div className="app">
-				<SideBar tasks={this.state.tasks} sendNotification={sendNotification} />
+				<SideBar tasks={this.props.tasks} sendNotification={sendNotification} />
 				<div className="mainBody">
-					<HoursOverlay tasks={this.state.tasks} />
+					<h1> ---------->{this.props.tasks.length}</h1>
+					<HoursOverlay tasks={this.props.tasks} />
 					<div className="tasksContainer">
-						{this.state.tasks.map(task => (
+						{this.props.tasks.map(task => (
 							<Task
 								key={task.id}
 								id={task.id}
@@ -358,12 +114,12 @@ export default class TaskController extends React.Component {
 								started={task.started}
 								paused={task.paused}
 								isViewing={task.isViewing}
-								taskOnClick={this.taskOnClick}
-								taskUp={this.taskUp}
-								taskDown={this.taskDown}
-								startTask={this.startTask}
-								finishTask={this.finishTask}
-								addTime={this.addTime}
+								taskOnClick={this.props.taskOnClick}
+								taskUp={this.props.taskUp}
+								taskDown={this.props.taskDown}
+								startTask={this.props.startTask}
+								finishTask={this.props.finishTask}
+								addTime={this.props.addTime}
 							/>
 						))}
 					</div>
