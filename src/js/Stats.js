66bfaec73571a45a732d3371b5628aeee9576763
nathan <nathannash11@gmail.com>
2020-04-23 19:37:55 +0100

    back to react vis aand basic chart

--- src/js/Stats.js
+++ src/js/Stats.js
@@ -1,57 +1,74 @@
 import React from 'react';
+import {XYPlot, ArcSeries, XAxis, YAxis} from 'react-vis';
+
 
 export default class Stats extends React.Component {
 
     render() {
         if (this.props.dayStats === null) return <h1>no stats</h1>;
 
+        const spacing = (Math.PI * 2) / 24;
+
         let startTimes = Object.keys(this.props.dayStats.startPoints);
         let endTimes = Object.keys(this.props.dayStats.startPoints);
         let i = 0;
         let data = [];
 
-        for (let j = 1; j <= 25;j++) {
-            let spacing = (Math.PI * 2) / 24;
+        for (let j = 1; j <= 24;j++) {
 
             let hour = {
-                angle: (j-1)*spacing,
-                angele0: j * spacing,
-                radius: 10,
-                radius0: 1,
+                angle0: (j-1)*spacing,
+                angle: j * spacing,
+                radius: 1,
+                radius0: 0,
                 stroke: 1,
-                color: i,
+                color: 1,
             };
 
             data.push(hour);
         }
-
-        console.log(data);
-
 
         let calcAngle = function (val) {
             let date = new Date(val);
             let minutes = (date.getHours() * 60) + date.getMinutes();
 
             //2PI = 24*60
-
-            return minutes / (24.0 * 60.0);
+            return minutes / (24.0 * 60.0) * (Math.PI * 2);
         };
-/*
+
         while (i < startTimes.length && i < endTimes.length) {
             let newData = {
-                angle: calcAngle(startTimes[i]),
-                angele0: 10 * calcAngle(endTimes[i]),
-                radius: 2,
-                radius0: 1
+                angle0: calcAngle(startTimes[i]),
+                angle: calcAngle(endTimes[i]),
+                radius: 1.5,
+                radius0: 0,
+                color: 0,
+                stroke: 0.1,
             };
 
             data.push(newData);
 
             i++;
         }
-*/
+
+        const COLORS = ['red', 'white'];
+
         return (
-            <h1>graph</h1>
+            <XYPlot
+                colorRange={COLORS}
+                xDomain={[-5, 5]}
+                yDomain={[-5, 5]}
+                width={600}
+                height={600}
+                style={{border:"1px solid red"}}
+            >
+                <XAxis />
+                <YAxis />
+                <ArcSeries
+                    animation
+                    data={data}
+                />
+            </XYPlot>
         );
     }
 }
