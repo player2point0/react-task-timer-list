a186114f1a3192fbcd7a78aa884d127210c5c27a
nathan <nathannash11@gmail.com>
2020-05-29 19:10:08 +0100

    fixed name

--- src/js/MainApp.js
+++ src/js/MainApp.js
@@ -131,7 +131,6 @@ export default class MainApp extends React.Component {
                 }));
 
                 this.createNewDayStats();
-                alert("new day");
             }
         }
     }
