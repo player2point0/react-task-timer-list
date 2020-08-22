93eecdef8d57513db7a82f403cf4f92b55875fae
nathan <nathannash11@gmail.com>
2020-08-22 13:16:55 +0100

    changed main app to func component and used redux

--- src/MainApp/MainApp.js
+++ src/MainApp/MainApp.js
@@ -1,213 +1,151 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../index.css";
 import "./auth.css";
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import TaskController from "../Task/TaskController";
 import DayRecap from "../DayRecap/DayRecap";
-import { useStoreState } from 'easy-peasy';
+import {useStoreState} from 'easy-peasy';
 
 import {
-    tick, addTask, updateTaskByIdFunc, taskOnClick, startTask, finishTask,
-    reportFlow,removeTaskWithId, addTime, saveAllTasks, completeObjective,
-    addObjective, setReportFlow,
+    tick, removeTaskWithId,
 } from "../Task/TaskController";
 
 import {
-    uiConfig, loadServerData, userAuthChanged,
-    firebaseSaveTask, firebaseSaveDayStat, firebaseSaveFeedback,
-    firebaseGetAllTasks, firebaseGetDayStat, getWeekStats,
-    parseSavedTasks, getCurrentUser, getAuth
+    uiConfig, userAuthChanged, getAuth
 } from "../Firebase/FirebaseController";
 
 import {formatDayMonth} from "../Utility/Utility";
 
 const SAVE_INTERVAL = 5 * 60 * 1000; //in milli for set interval
 
-export default class MainApp extends React.Component {
+export default function MainApp() {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            tasks: [],
-            showAuthHtml: (getCurrentUser() === null),
-            lastTickTime: new Date(),
-            removeTaskId: "",
-            setSaveAllTasks: false,
-            dayStat: null,
-            weekDayStats: null,
-            showRecap: false,
-        };
-
+    const showAuthHtml = useStoreState(state => state.showAuthHtml);
+    const [showRecap, setShowRecap] = useState(false);
+    /*
         this.toggleDayRecap = this.toggleDayRecap.bind(this);
         this.tick = tick.bind(this);
-        this.saveAllTasks = saveAllTasks.bind(this);
-        this.addTask = addTask.bind(this);
-        this.taskOnClick = taskOnClick.bind(this);
-        this.startTask = startTask.bind(this);
-        this.finishTask = finishTask.bind(this);
-        this.addTime = addTime.bind(this);
-        this.reportFlow = reportFlow.bind(this);
-        this.setReportFlow = setReportFlow.bind(this);
-        this.completeObjective = completeObjective.bind(this);
-        this.addObjective = addObjective.bind(this);
         this.removeTaskWithId = removeTaskWithId.bind(this);
-        this.updateTaskByIdFunc = updateTaskByIdFunc.bind(this);
         this.createNewDayStat = this.createNewDayStat.bind(this);
         this.userAuthChanged = userAuthChanged.bind(this);
-        this.firebaseSaveTask = firebaseSaveTask.bind(this);
-        this.firebaseSaveDayStat = firebaseSaveDayStat.bind(this);
-        this.firebaseSaveFeedback = firebaseSaveFeedback.bind(this);
-        this.firebaseGetAllTasks = firebaseGetAllTasks.bind(this);
-        this.firebaseGetDayStat = firebaseGetDayStat.bind(this);
-        this.getWeekStats = getWeekStats.bind(this);
-        this.parseSavedTasks = parseSavedTasks.bind(this);
-        this.loadServerData = loadServerData.bind(this);
-    }
+    */
+    /*
+        componentDidUpdate(prevProps, prevState, snapshot) {
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-
-        if (this.state.removeTaskId) {
-            this.removeTaskWithId(this.state.removeTaskId);
-            this.setState({removeTaskId: ""});
-        }
-
-        if (this.state.setSaveAllTasks) {
-
-            //grab the tasks that have changed
-            let taskToSave = [];
-            let currentTasks = this.state.tasks.slice();
-
-            for (let i = 0; i < currentTasks.length; i++) {
-                if (currentTasks[i].needsSaved) {
-                    taskToSave.push(currentTasks[i]);
-                    currentTasks[i].needsSaved = false;
-                }
+            if (this.state.removeTaskId) {
+                this.removeTaskWithId(this.state.removeTaskId);
+                this.setState({removeTaskId: ""});
             }
 
-            this.saveAllTasks(taskToSave);
+            if (this.state.setSaveAllTasks) {
 
-            this.setState({
-                setSaveAllTasks: false,
-                task: currentTasks
-            });
-        }
+                //grab the tasks that have changed
+                let taskToSave = [];
+                let currentTasks = this.state.tasks.slice();
 
-        //check if the day has changed
-        let currentDate = formatDayMonth(new Date());
-        if (this.state.dayStat !== null) {
-            if (this.state.dayStat.date !== currentDate) {
-                //update the week stats by one day
-                let newWeekDayStats = this.state.weekDayStats.slice();
-                //remove the oldest date from the end
-                newWeekDayStats.pop();
-                //add the latest day stat to the front
-                newWeekDayStats.unshift(this.state.dayStat);
+                for (let i = 0; i < currentTasks.length; i++) {
+                    if (currentTasks[i].needsSaved) {
+                        taskToSave.push(currentTasks[i]);
+                        currentTasks[i].needsSaved = false;
+                    }
+                }
+
+                this.saveAllTasks(taskToSave);
 
                 this.setState({
-                    weekDayStats: newWeekDayStats,
+                    setSaveAllTasks: false,
+                    task: currentTasks
                 });
+            }
 
-                this.createNewDayStat();
+            //check if the day has changed
+            let currentDate = formatDayMonth(new Date());
+            if (this.state.dayStat !== null) {
+                if (this.state.dayStat.date !== currentDate) {
+                    //update the week stats by one day
+                    let newWeekDayStats = this.state.weekDayStats.slice();
+                    //remove the oldest date from the end
+                    newWeekDayStats.pop();
+                    //add the latest day stat to the front
+                    newWeekDayStats.unshift(this.state.dayStat);
+
+                    this.setState({
+                        weekDayStats: newWeekDayStats,
+                    });
+
+                    this.createNewDayStat();
+                }
             }
         }
-    }
 
-    // Listen to the Firebase Auth state and set the local state.
-    //interval for the tick method, called when changes are made to the props
-    componentDidMount() {
+        // Listen to the Firebase Auth state and set the local state.
+        //interval for the tick method, called when changes are made to the props
+        componentDidMount() {
 
-        if (this.state.dayStat === null) {
-            this.createNewDayStat();
+            if (this.state.dayStat === null) {
+                this.createNewDayStat();
+            }
+
+            if (this.state.weekDayStats === null) {
+                this.setState(state => ({
+                    weekDayStats: [state.dayStat]
+                }))
+            }
+
+            this.unregisterAuthObserver = getAuth().onAuthStateChanged(this.userAuthChanged);
+            this.tickInterval = setInterval(() => this.tick(), 1000);
+            this.saveInterval = setInterval(() => this.setState({setSaveAllTasks: true}), SAVE_INTERVAL);
         }
 
-        if (this.state.weekDayStats === null) {
-            this.setState(state => ({
-                weekDayStats: [state.dayStat]
-            }))
+        // Make sure we un-register Firebase observers when the component unmounts.
+        componentWillUnmount() {
+            this.unregisterAuthObserver();
+            clearInterval(this.tickInterval);
+            clearInterval(this.saveInterval);
         }
 
-        this.unregisterAuthObserver = getAuth().onAuthStateChanged(this.userAuthChanged);
-        this.tickInterval = setInterval(() => this.tick(), 1000);
-        this.saveInterval = setInterval(() => this.setState({setSaveAllTasks: true}), SAVE_INTERVAL);
-    }
+        createNewDayStat() {
+            let newDayStat = {
+                date: formatDayMonth(new Date()),
+                totalWorked: 0,
+                totalBreak: 0,
+                userId: null,
+                tasks: [],
+            };
 
-    // Make sure we un-register Firebase observers when the component unmounts.
-    componentWillUnmount() {
-        this.unregisterAuthObserver();
-        clearInterval(this.tickInterval);
-        clearInterval(this.saveInterval);
-    }
-
-    createNewDayStat() {
-        let newDayStat = {
-            date: formatDayMonth(new Date()),
-            totalWorked: 0,
-            totalBreak: 0,
-            userId: null,
-            tasks: [],
-        };
-
-        this.setState({
-            dayStat: newDayStat,
-            setSaveAllTasks: true,
-        });
-    }
-
-    toggleDayRecap() {
-        this.setState(state => ({
-            showRecap: !state.showRecap,
-        }));
-    }
-
-    render() {
-        //todo refactor this to a more advanced system of display content
-        // one that could also be expanded to work with a a/b system
-        // maybe using the state / redux state if it is added
-        let authHtml;
-        let weekDayStats = this.state.weekDayStats;
-
-        if (weekDayStats) {
-            weekDayStats = weekDayStats.slice();
-            //swap day stat loaded from the server with the current one
-            weekDayStats.shift();
-            weekDayStats.unshift(this.state.dayStat);
+            this.setState({
+                dayStat: newDayStat,
+                setSaveAllTasks: true,
+            });
         }
+    */
+    const toggleDayRecap = () => {
+        setShowRecap(!showRecap);
+    };
 
-        if (this.state.showAuthHtml) {
-            authHtml = <div className={"authContainer"}>
-                <div className={"authTitle"}>sign in / up</div>
-                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={getAuth()}/>
-            </div>;
-        }
+    //todo refactor this to a more advanced system of display content
+    // one that could also be expanded to work with a a/b system
+    // maybe using the state / redux state if it is added
+    let authHtml;
 
-        return (
-            <div>
-                    <div
-                        className={"dayRecapButton"}
-                        onClick={this.toggleDayRecap}
-                    >
-                        recap
-                    </div>
-                {this.state.showRecap? <DayRecap
-                    dayStat={this.state.dayStat}
-                />:""}
-                {authHtml}
-                {!this.state.showRecap?<TaskController
-                    firebaseSaveTask={this.firebaseSaveTask}
-                    firebaseGetAllTasks={this.firebaseGetAllTasks}
-                    tasks={this.state.tasks}
-                    addTask={this.addTask}
-                    taskOnClick={this.taskOnClick}
-                    startTask={this.startTask}
-                    finishTask={this.finishTask}
-                    setReportFlow={this.setReportFlow}
-                    reportFlow={this.reportFlow}
-                    addTime={this.addTime}
-                    completeObjective={this.completeObjective}
-                    addObjective={this.addObjective}
-                />:""}
+    if (showAuthHtml) {
+        authHtml = <div className={"authContainer"}>
+            <div className={"authTitle"}>sign in / up</div>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={getAuth()}/>
+        </div>;
+    }
+
+    return (
+        <div>
+            <div
+                className={"dayRecapButton"}
+                onClick={toggleDayRecap}
+            >
+                recap
             </div>
-        );
-    }
+            {showRecap && <DayRecap/>}
+            {authHtml}
+            {!showRecap && <TaskController/>}
+        </div>
+    );
 }
