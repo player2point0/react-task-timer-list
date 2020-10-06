246ebbd565d70903ceb95c8c5fbf41b0e18b9f05
nathan <nathannash11@gmail.com>
2020-10-06 23:36:51 +0100

    basic flow slider

--- src/Task/FlowReport/FlowReport.js
+++ src/Task/FlowReport/FlowReport.js
@@ -1,96 +1,78 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState,} from 'react';
 import "./flowReport.css";
 import {useStoreState} from "easy-peasy";
 
-const INPUT_SIDE_SCALE = 0.75;
-
 export default function FlowReport({onDone, id}) {
-    const screenWidth = window.innerWidth
-        || document.documentElement.clientWidth
-        || document.body.clientWidth;
-    const screenHeight = window.innerHeight
-        || document.documentElement.clientHeight
-        || document.body.clientHeight;
-
-    const inputWidth = screenWidth * INPUT_SIDE_SCALE;
-    const inputHeight = screenHeight * INPUT_SIDE_SCALE;
-    const inputSideLength = inputWidth < inputHeight ? inputWidth : inputHeight;
-
-    const inputPinDim = 50;
 
     const dayStat = useStoreState(state => state.dayStat.dayStat);
-    const flowLen =  dayStat.flow.length;
-    const previousFocus = flowLen > 0? dayStat.flow[flowLen-1].focus:0.5;
-    const previousProductive = flowLen > 0? dayStat.flow[flowLen-1].productive:0.5;
+    const flowLen = dayStat.flow.length;
+    const previousFocus = flowLen > 0 ? dayStat.flow[flowLen - 1].focus : 0.5;
+    const previousProductive = flowLen > 0 ? dayStat.flow[flowLen - 1].productive : 0.5;
+    const [focus, setFocus] = useState(previousFocus*100);
+    const [productive, setProductive] = useState(previousProductive*100);
 
-    const [inputPinCoords, setInputPinCoords] = useState({
-        x: "calc("+previousFocus*100+"% - "+(inputPinDim/2)+"px)",
-        y: "calc("+(1-previousProductive)*100+"% - 25px)"
-    });
+    const focusStates = ["nah", "kinda", "yep"];
+    const productiveStates = ["nah", "kinda", "yep"];
 
-    const flowInputOnClick = (e) => {
-        const distX = (screenWidth - inputSideLength) / 2;
-        const distY = (screenHeight - inputSideLength) / 2;
-
-        const focused = (e.clientX - distX) / inputSideLength;
-        const productive = 1 - (e.clientY - distY) / inputSideLength;
-
-        const focusedRounded = focused.toFixed(2);
-        const productiveRounded = productive.toFixed(2);
-
-        const xPer = (focusedRounded * 100)+"%";
-        const yPer = ((1-productiveRounded) * 100)+"%";
-
-        setInputPinCoords({
-            x: "calc("+xPer+" - "+(inputPinDim/2)+"px)",
-            y: "calc("+yPer+" - "+(inputPinDim/2)+"px)"
-        });
-
-        onDone({
-            id: id,
-            productive: productiveRounded,
-            focus: focusedRounded,
-        });
+    const mapValue = (val, in_min, in_max, out_min, out_max) => {
+        return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     };
+
+    const getSliderLabel = (sliderValue, labelStates) => {
+        const index = Math.floor(mapValue(sliderValue, 0, 101, 0, labelStates.length));
+        return labelStates[index];
+    };
+
+    //todo add flow zone indicator when high focus and producitvity
 
     return (
         <div className={"flowBody"}>
-            <div
-                className={"flowNA"}
-                onClick={() => onDone({
-                    id: id,
-                    productive: "",
-                    focus: "",
-                })}
-            >
-                n/a
+            <div>
+                focus: {getSliderLabel(focus, focusStates)}
             </div>
+            <input
+                type="range"
+                min="0"
+                max="100"
+                className="flowSlider"
+                value={focus}
+                onChange={(e) => setFocus(e.target.value)}
+            />
+            <div>
+                productive: {getSliderLabel(productive, productiveStates)}
+            </div>
+            <input
+                type="range"
+                min="0"
+                max="100"
+                className="flowSlider"
+                value={productive}
+                onChange={(e) => setProductive(e.target.value)}
+            />
             <div
-                className={"flowInput"}
-                onClick={flowInputOnClick}
-                style={{
-                    width: inputSideLength,
-                    height: inputSideLength
-                }}
+                className={"flowButtons"}
             >
                 <div
-                    style={{
-                        left: inputPinCoords.x,
-                        top: inputPinCoords.y
-                    }}
-                    className={"flowInputPin"}
-                    draggable
-                />
-                <div className={"flowInputLabelFlow flowInputLabel"}>
-                    flow
+                    className={"naButton"}
+                    onClick={() => onDone({
+                        id: id,
+                        productive: "",
+                        focus: "",
+                    })}
+                >
+                    n/a
                 </div>
-                <div className={"flowInputLabelFocused flowInputLabel"}>
-                    focused
-                </div>
-                <div className={"flowInputLabelProductive flowInputLabel"}>
-                    p<br/>r<br/>o<br/>d<br/>u<br/>c<br/>t<br/>i<br/>v<br/>e
+                <div
+                    className={"doneButton"}
+                    onClick={() => onDone({
+                        id: id,
+                        productive: (productive/100).toFixed(2),
+                        focus: (focus/100).toFixed(2),
+                    })}
+                >
+                    done
                 </div>
             </div>
         </div>
-    );
+);
 }
