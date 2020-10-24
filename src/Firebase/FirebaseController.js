845fbed4a0ebdd4de9fe50144e1b039f5e214184
nathan <nathannash11@gmail.com>
2020-10-24 19:37:31 +0100

    old logins

--- src/Firebase/FirebaseController.js
+++ src/Firebase/FirebaseController.js
@@ -39,12 +39,6 @@ export const uiConfig = {
     // We will display Google and Facebook as auth providers.
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        {
-            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            // Whether the display name should be displayed in the Sign Up page.
-            requireDisplayName: false
-        }
     ],
     callbacks: {
         // Avoid redirects after sign-in.
