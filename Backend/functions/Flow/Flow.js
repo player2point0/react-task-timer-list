e10cba62ea9eeada47ee3e4a4aba1525fecacfd4
nathan <nathannash11@gmail.com>
2020-10-26 20:00:17 +0000

    skeleton be jobs

---
+++ Backend/functions/Flow/Flow.js
@@ -0,0 +1,10 @@
+exports.calcFlowTimes = () => {
+    //load in the existing flow times from userData
+    //load in the dayStats for the past week
+
+    //calculate for 3 hour preiods
+    //Mon 9-12, 12-15, 15-18, 18-21, 21-24
+
+    //update average value
+
+};
---
+++ Backend/functions/Tags/Tags.js
@@ -0,0 +1,4 @@
+exports.calcAvgTagTime = () => {
+    //for each task in the past week
+    //update the average tag time kwith the task duration
+}
