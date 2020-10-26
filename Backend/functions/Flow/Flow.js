7c7246dd1e09aee12c1a4f0953c45e665940ccab
nathan <nathannash11@gmail.com>
2020-10-26 21:38:41 +0000

    work on firebase logic

--- Backend/functions/Flow/Flow.js
+++ Backend/functions/Flow/Flow.js
@@ -1,10 +1,8 @@
-exports.calcFlowTimes = () => {
-    //load in the existing flow times from userData
-    //load in the dayStats for the past week
-
+exports.calcFlowTimes = (dayStats, userData) => {
     //calculate for 3 hour preiods
     //Mon 9-12, 12-15, 15-18, 18-21, 21-24
 
     //update average value
+    //return updated userData
 
 };
--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,7 +1,10 @@
 const Tasks = require("./Tasks/Tasks");
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const moment = require('moment');
 admin.initializeApp();
+
+const CRON_EVERY_MON_AT_FIVE_AM = "'0 5 * * 1";
 
 //todo alternatively could add a user tabel that contains the tasks to load, could be updated hourly
 exports.loadTasks = functions.https.onCall(async (data, context) => {
@@ -15,3 +18,53 @@ exports.loadTasks = functions.https.onCall(async (data, context) => {
             return Tasks.sortTasks(tasks);
         });
 });
+
+//runs every mone
+exports.calcFlowTimes = functions.pubsub.schedule(CRON_EVERY_MON_AT_FIVE_AM)
+    .timeZone('Europe/Dublin')
+    .onRun((context) => {
+        const weekAgo = moment().subtract(7, 'days');
+        //get all dayStats from the past week
+        const dayStatsPromise = admin.firestore().collection('dayStats')
+            .where("date", ">", weekAgo)
+            .orderBy("userId")
+            .get()
+            .then(snapshot => snapshot.docs.map(doc => doc.data()));
+        //group by userId
+        const groupedDayStatsPromise = dayStatsPromise.then(dayStats => {
+            return dayStats.reduce((r, a) => {
+                r[a.userId] = r[a.userId] || [];
+                r[a.userId].push(a);
+                return r;
+            }, Object.create(null));
+        })
+        //get the userData for the dayStats
+        const userDataAndGroupedDayStatPromise = groupedDayStatsPromise.then(groupedDayStat => {
+
+            //go through all the groups
+            //get the user data or null
+            const userId = groupedDayStat[0].userId;
+            //todo add default here i think
+            const userDataPromise = admin.firestore().collection('userData')
+                .doc(userId)
+                .get()
+                .then(snapshot => snapshot.data());
+
+            return {
+                dayStats,
+                userDataPromise,
+            };
+        })
+        //call flow.calcFlowTimes
+        const val = userDataAndGroupedDayStatPromise.then(group => {
+            const [dayStat, userData] = group;
+
+            Flow.calcFlowTimes(dayStat, userData);
+
+            return 1;
+        })
+        //update the userData table if a result is returned
+
+        console.log('');
+        return null;
+    });
