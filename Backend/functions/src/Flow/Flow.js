819da7f4b41456b36c0831aa22fcf9eae7f4b836
nathan <nathannash11@gmail.com>
2020-11-01 00:33:18 +0000

    ts gitignore bug

---
+++ Backend/functions/src/Flow/Flow.js
@@ -0,0 +1,17 @@
+const MONDAY = 1;
+
+exports.calcFlowTimes = (dayStats, userData) => {
+	dayStats.array.forEach(dayStat => {
+		//find out what day the current dayStat belongs to
+		const dayOfWeek = dayStat.date;
+		//create the day if it doesn't exist
+		//go through each task in dayStat
+
+		//calculate for 3 hour periods
+		//Mon 9-12, 12-15, 15-18, 18-21, 21-24
+		//update average value
+		//return updated userData
+	});
+};
+
+const getDayOfWeek = date => {};
---
+++ Backend/functions/src/Flow/Flow.test.js
@@ -0,0 +1,12 @@
+const Flow = require("./Flow")
+const data = require("./Flow.test.data")
+
+describe("calculates flow times for a user", () => {
+    it("returns updated userData when dayStats provided", () => {
+
+    });
+
+    it("calculates the average properly", () => {
+
+    });
+});
